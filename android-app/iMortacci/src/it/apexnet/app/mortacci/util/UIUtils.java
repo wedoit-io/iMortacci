cab48cf1032ee9e21efff39940ed7262cb41c673
f.nolano <f.nolano@a0f7fa0d-9257-9c46-9e05-140798707ebc>
2011-05-17 15:49:15 +0000

    iMortacci, android: * i nserita funzione di share mortaccione

--- android-app/iMortacci/src/it/apexnet/app/mortacci/util/UIUtils.java
+++ android-app/iMortacci/src/it/apexnet/app/mortacci/util/UIUtils.java
@@ -1,5 +1,6 @@
 package it.apexnet.app.mortacci.util;
 
+import it.apexnet.app.mortacci.R;
 import it.apexnet.app.mortacci.ui.AlbumActivity;
 import android.content.Context;
 import android.content.Intent;
@@ -12,5 +13,17 @@ public class UIUtils {
         final Intent intent = new Intent(context, AlbumActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         context.startActivity(intent);
+    }
+    
+    public static void share(String title, String subject, String text, Context context)
+    {
+		final Intent intent = new Intent(Intent.ACTION_SEND);			
+		
+		intent.setType("text/plain");
+		intent.putExtra(Intent.EXTRA_TITLE, title);
+		intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+		intent.putExtra(Intent.EXTRA_TEXT, text);		
+		 
+		context.startActivity(Intent.createChooser(intent,  context.getResources().getString(R.string.share)));
     }
 }
